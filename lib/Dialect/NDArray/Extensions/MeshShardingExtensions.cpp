e5b5a2e2b414576ebc039a9fa22003880f782236
Schlimbach, Frank <frank.schlimbach@intel.com>
2024-08-28 17:53:50 +0200

    sharding propagation for ndarray.insert_slice

--- lib/Dialect/NDArray/Extensions/MeshShardingExtensions.cpp
+++ lib/Dialect/NDArray/Extensions/MeshShardingExtensions.cpp
@@ -23,11 +23,90 @@ using namespace imex::ndarray;
 using namespace mlir::mesh;
 
 namespace {
+// Computes result sharding by extending a copy of the input sharding with shard sizes.
+// The shard sizes reflect the sizes resulting from the non-copying subview operation.
+// Requires sharding of input tensor.
+FailureOr<MeshSharding> getShardedDimsSizes(Value ary, OffsetSizeAndStrideOpInterface op) {
+  SymbolTableCollection symbolTable;
+  auto aryType = cast<RankedTensorType>(ary.getType());
+  // currently no support for dynamic input shapes
+  if (!aryType.hasStaticShape())
+    return failure();
+  auto aryShape = aryType.getShape();
+  auto rank = cast<RankedTensorType>(ary.getType()).getRank();
+
+  auto aryShardOp = ary.getDefiningOp<mesh::ShardOp>();
+  // currently no support for non-sharded source
+  if (!aryShardOp)
+    return failure();
+
+  auto offs = op.getStaticOffsets();
+  auto sizes = op.getStaticSizes();
+  auto strides = op.getStaticStrides();
+  // currently no support for dynamic subviews
+  if (ShapedType::isDynamicShape(offs) ||
+      ShapedType::isDynamicShape(sizes) ||
+      ShapedType::isDynamicShape(strides))
+    return failure();
+
+  auto arySharding = aryShardOp.getSharding().getDefiningOp<mesh::ShardingOp>();
+  // currently no support for sharding dims sizes on input
+  if(!arySharding.getStaticShardedDimsSizes().empty())
+    return failure();
+
+  auto mesh = getMesh(arySharding, symbolTable);
+  if (!mesh)
+    return failure();
+  auto meshShape = mesh.getShape();
+  // currently no support for dynamic mesh shape
+  if (llvm::any_of(meshShape, [](int64_t dim) { return dim == ShapedType::kDynamic; }))
+    return failure();
+
+  auto splitAxes = arySharding.getSplitAxes();
+  assert((int64_t)splitAxes.size() <= rank);
+  
+  // flattened shard sizes for each dimension (see sharding.sharded_dims_sizes) after subview
+  SmallVector<int64_t> splitSzs;
+  // iterate split tensor dimensions
+  for(auto dim = 0u; dim<splitAxes.size(); ++dim) {
+    auto axes = arySharding.getSplitAxes().getAxes()[dim].asArrayRef();
+    if (axes.empty()) continue;
+    int64_t splitSz = 1;  // number of shards in this dimension
+    for(auto i : axes) splitSz *= meshShape[i];
+    int64_t shardSz = shardDimension(aryShape[dim], splitSz);
+    int64_t pos = offs[dim]; // current position in split tensor dim
+    int64_t mx = sizes[dim]; // max number of elements we assign to current shard
+    for(auto shard = 0; shard < splitSz; ++shard) {
+      // extract size of overlap of subview with current input shard
+      auto shardStart = shard * shardSz;
+      auto shardEnd = shardStart + shardSz;
+      auto num = shardEnd - pos;
+      int64_t sz = 0;
+      if (num > 0) { // if starts before end of shard
+        sz = (num + (strides[dim]-1)) / strides[dim];
+        sz = std::min(mx, sz);
+      }
+      splitSzs.emplace_back(sz);
+      // update pos and max for next result shard
+      pos += sz * strides[dim];
+      mx -= sz;
+    }
+  }
+  return MeshSharding::get(arySharding.getMeshAttr(),
+                            arySharding.getSplitAxes().getAxes(),
+                            arySharding.getPartialAxes().value_or(llvm::ArrayRef<MeshAxis>{}),
+                            arySharding.getPartialType().value_or(ReductionKind::Sum),
+                            {}, // static halo
+                            splitSzs,
+                            {},
+                            {});
+}
 
 // Sharding of tensor.empty
-struct SubViewShardingInterface
-    :  public ShardingInterface::ExternalModel<SubViewShardingInterface, imex::ndarray::SubviewOp> {
-  using parent = ShardingInterface::ExternalModel<SubViewShardingInterface, imex::ndarray::SubviewOp>;
+template<typename T, typename OpType>
+struct OffsetSizeAndStrideShardingInterface
+    :  public ShardingInterface::ExternalModel<T, OpType> {
+  using parent = ShardingInterface::ExternalModel<T, OpType>;
   
   SmallVector<mlir::utils::IteratorType> getLoopIteratorTypes(::mlir::Operation *op) const {
     LLVM_DEBUG(DBGS() << "getLoopIteratorTypes\n");
@@ -40,12 +119,6 @@ struct SubViewShardingInterface
     return types;
   }
   
-  SmallVector<ReductionKind> getReductionLoopIteratorKinds(::mlir::Operation *op) const
-  {
-    LLVM_DEBUG(DBGS() << "getReductionLoopIteratorKinds\n");
-    return parent::getReductionLoopIteratorKinds(op);
-  }
-
   SmallVector<AffineMap> getIndexingMaps(::mlir::Operation *op) const {
     LLVM_DEBUG(DBGS() << "getIndexingMaps\n");
     MLIRContext *ctx = op->getContext();
@@ -60,106 +133,6 @@ struct SubViewShardingInterface
     return maps;
   }
 
-  FailureOr<ShardingOption> getShardingOption(::mlir::Operation *op, ArrayRef<MeshSharding> operandShardings, ArrayRef<MeshSharding> resultShardings) const {
-    LLVM_DEBUG(DBGS() << "getShardingOption\n");
-    return parent::getShardingOption(op, operandShardings, resultShardings);
-  }
-
-  FailureOr<std::vector<MeshSharding>> getShardingAnnotations(::mlir::Operation *op, const ShardingOption &shardingOption) const {
-    LLVM_DEBUG(DBGS() << "getShardingAnnotations\n");
-    return parent::getShardingAnnotations(op, shardingOption);
-  }
-
-  LogicalResult addShardingAnnotations(::mlir::Operation *op, OpBuilder &b, const ShardingOption &shardingOption) const {
-    LLVM_DEBUG(DBGS() << "addShardingAnnotations\n");
-    // return parent::addShardingAnnotations(op, b, shardingOption);
-    SymbolTableCollection symbolTable;
-    auto svop = cast<SubviewOp>(op);
-    auto ary = svop.getSource();
-    auto aryType = cast<RankedTensorType>(ary.getType());
-    if (!aryType.hasStaticShape())
-      return failure(); // currently no support for dynamic input shapes
-    auto aryShape = aryType.getShape();
-    auto rank = cast<RankedTensorType>(ary.getType()).getRank();
-
-    auto aryShardOp = ary.getDefiningOp<mesh::ShardOp>();
-    if (!aryShardOp)
-      return failure(); // currently no support for non-sharded source
-
-    auto offs = svop.getStaticOffsets();
-    auto sizes = svop.getStaticSizes();
-    auto strides = svop.getStaticStrides();
-    if (ShapedType::isDynamicShape(offs) ||
-        ShapedType::isDynamicShape(sizes) ||
-        ShapedType::isDynamicShape(strides))
-      return failure(); // currently no support for dynamic subviews
-
-    auto arySharding = aryShardOp.getSharding().getDefiningOp<mesh::ShardingOp>();
-    if(!arySharding.getStaticShardedDimsSizes().empty())
-      return failure(); // currently no support for sharding dims sizes on input
-
-    auto mesh = getMesh(arySharding, symbolTable);
-    if (!mesh)
-      return failure();
-    auto meshShape = mesh.getShape();
-    if (llvm::any_of(meshShape, [](int64_t dim) { return dim == ShapedType::kDynamic; }))
-      return failure();
-    
-    SmallVector<int64_t> meshTileSzs(meshShape.size(), 1);
-    for (int64_t dim = 0; dim<(int64_t)meshTileSzs.size(); ++dim) {
-      for (auto dim=(int64_t)meshTileSzs.size()-2; dim>=0; --dim) {
-        meshTileSzs[dim] = meshShape[dim] * meshTileSzs[dim+1];
-      }
-    }
-
-    SmallVector<bool> definedAxis(meshShape.size(), false);
-    auto splitAxes = arySharding.getSplitAxes();
-    assert((int64_t)splitAxes.size() <= rank);
-    auto numSplitAxes = 0;
-    for (auto [d, axes] : llvm::enumerate(splitAxes)) {
-      if (axes.size()) {
-        ++numSplitAxes;
-        definedAxis[d] = true;
-      }
-    }
-    
-    SmallVector<int64_t> splitSzs;
-    for(auto dim = 0u; dim<splitAxes.size(); ++dim) {
-      auto axes = arySharding.getSplitAxes().getAxes()[dim].asArrayRef();
-      if (axes.empty()) continue;
-      int64_t splitSz = 1;
-      for(auto i : axes) splitSz *= meshShape[i];
-      int64_t shardSz = shardDimension(aryShape[dim], splitSz);
-      int64_t pos = offs[dim];
-      int64_t mx = sizes[dim];
-      for(auto shard = 0; shard < splitSz; ++shard) {
-        auto shardStart = shard * shardSz;
-        auto shardEnd = shardStart + shardSz;
-        auto num = shardEnd - pos;
-        int64_t sz = 0;
-        if (num > 0) {
-          sz = (num + (strides[dim]-1)) / strides[dim];
-          sz = std::min(mx, sz);
-        }
-        splitSzs.emplace_back(sz);
-        pos += sz * strides[dim];
-        mx -= sz;
-      }
-    }
-
-    auto resSharding = MeshSharding::get(arySharding.getMeshAttr(),
-                                         arySharding.getSplitAxes().getAxes(),
-                                         arySharding.getPartialAxes().value_or(llvm::ArrayRef<MeshAxis>{}),
-                                         arySharding.getPartialType().value_or(ReductionKind::Sum),
-                                         {}, // static halo
-                                         splitSzs,
-                                         {},
-                                         {});
-    maybeInsertTargetShardingAnnotation(resSharding, op->getResult(0), b);
-
-    return success();
-  }
-
   LogicalResult spmdize(::mlir::Operation *op, ArrayRef<Value> spmdizedOperands, ArrayRef<MeshSharding> operandShardings, ArrayRef<MeshSharding> resultShardings, IRMapping&spmdizationMap, SymbolTableCollection &symbolTableCollection, OpBuilder &builder) const {
     LLVM_DEBUG(DBGS() << "spmdize\n");
     spmdizeTriviallyShardableOperation(*op, spmdizedOperands, operandShardings,
@@ -168,10 +141,35 @@ struct SubViewShardingInterface
     return success();
   }
 };
+
+struct SubviewShardingInterface : public OffsetSizeAndStrideShardingInterface<SubviewShardingInterface, imex::ndarray::SubviewOp> {
+  LogicalResult addShardingAnnotations(::mlir::Operation *op, OpBuilder &b, const ShardingOption &shardingOption) const {
+    LLVM_DEBUG(DBGS() << "addShardingAnnotations\n");
+    auto svop = cast<SubviewOp>(op);
+    auto sharding = getShardedDimsSizes(svop.getSource(), svop);
+    if(failed(sharding)) return failure();
+    maybeInsertTargetShardingAnnotation(sharding.value(), op->getResult(0), b);
+
+    return success();
+  }
+};
+
+struct InsertSliceShardingInterface : public OffsetSizeAndStrideShardingInterface<InsertSliceShardingInterface, imex::ndarray::InsertSliceOp> {
+  LogicalResult addShardingAnnotations(::mlir::Operation *op, OpBuilder &b, const ShardingOption &shardingOption) const {
+    LLVM_DEBUG(DBGS() << "addShardingAnnotations\n");
+    auto svop = cast<InsertSliceOp>(op);
+    auto sharding = getShardedDimsSizes(svop.getDestination(), svop);
+    if(failed(sharding)) return failure();
+    maybeInsertSourceShardingAnnotation(sharding.value(), op->getOpOperand(1), b);
+
+    return success();
+  }
+};
 } // namespace
 
 void imex::ndarray::registerShardingInterfaceExternalModels(DialectRegistry &registry) {
   registry.addExtension(+[](MLIRContext *ctx, imex::ndarray::NDArrayDialect *dialect) {
-    SubviewOp::template attachInterface<SubViewShardingInterface>(*ctx);
+    SubviewOp::template attachInterface<SubviewShardingInterface>(*ctx);
+    InsertSliceOp::template attachInterface<InsertSliceShardingInterface>(*ctx);
   });
 }
--- test/Dialect/NDArray/Extensions/sharding_propagation.mlir
+++ test/Dialect/NDArray/Extensions/sharding_propagation.mlir
@@ -3,8 +3,8 @@
 builtin.module {
     
 mesh.mesh @mesh4(shape = 4)
-// CHECK-LABEL: @test_shard_propagate_balanced
-func.func @test_shard_propagate_balanced(%arg0: tensor<1024x1024xi64>) -> tensor<4x3xi64> {
+// CHECK-LABEL: @test_shard_propagate_subview_balanced
+func.func @test_shard_propagate_subview_balanced(%arg0: tensor<1024x1024xi64>) -> tensor<4x3xi64> {
     // CHECK: [[S:%.*]] = mesh.sharding @mesh4 split_axes = {{\[\[}}0]] : !mesh.sharding
     %s = mesh.sharding @mesh4 split_axes = [[0]] : !mesh.sharding
     // CHECK: mesh.shard %arg0 to [[S]] : tensor<1024x1024xi64>
@@ -14,8 +14,8 @@ func.func @test_shard_propagate_balanced(%arg0: tensor<1024x1024xi64>) -> tensor
     return %1 : tensor<4x3xi64>
 }
 
-// CHECK-LABEL: @test_shard_propagate_leading
-func.func @test_shard_propagate_leading(%arg0: tensor<1024x1024xi64>) -> tensor<3x3xi64> {
+// CHECK-LABEL: @test_shard_propagate_subview_leading
+func.func @test_shard_propagate_subview_leading(%arg0: tensor<1024x1024xi64>) -> tensor<3x3xi64> {
     // CHECK: [[S:%.*]] = mesh.sharding @mesh4 split_axes = {{\[\[}}0]] : !mesh.sharding
     %s = mesh.sharding @mesh4 split_axes = [[0]] : !mesh.sharding
     // CHECK: mesh.shard %arg0 to [[S]] : tensor<1024x1024xi64>
@@ -25,8 +25,8 @@ func.func @test_shard_propagate_leading(%arg0: tensor<1024x1024xi64>) -> tensor<
     return %1 : tensor<3x3xi64>
 }
 
-// CHECK-LABEL: @test_shard_propagate_mid
-func.func @test_shard_propagate_mid(%arg0: tensor<1024x1024xi64>) -> tensor<3x3xi64> {
+// CHECK-LABEL: @test_shard_propagate_subview_mid
+func.func @test_shard_propagate_subview_mid(%arg0: tensor<1024x1024xi64>) -> tensor<3x3xi64> {
     // CHECK: [[S:%.*]] = mesh.sharding @mesh4 split_axes = {{\[\[}}0]] : !mesh.sharding
     %s = mesh.sharding @mesh4 split_axes = [[0]] : !mesh.sharding
     // CHECK: mesh.shard %arg0 to [[S]] : tensor<1024x1024xi64>
@@ -36,8 +36,8 @@ func.func @test_shard_propagate_mid(%arg0: tensor<1024x1024xi64>) -> tensor<3x3x
     return %1 : tensor<3x3xi64>
 }
 
-// CHECK-LABEL: @test_shard_propagate_trailing
-func.func @test_shard_propagate_trailing(%arg0: tensor<1024x1024xi64>) -> tensor<3x3xi64> {
+// CHECK-LABEL: @test_shard_propagate_subview_trailing
+func.func @test_shard_propagate_subview_trailing(%arg0: tensor<1024x1024xi64>) -> tensor<3x3xi64> {
     // CHECK: [[S:%.*]] = mesh.sharding @mesh4 split_axes = {{\[\[}}0]] : !mesh.sharding
     %s = mesh.sharding @mesh4 split_axes = [[0]] : !mesh.sharding
     // CHECK: mesh.shard %arg0 to [[S]] : tensor<1024x1024xi64>
@@ -47,8 +47,8 @@ func.func @test_shard_propagate_trailing(%arg0: tensor<1024x1024xi64>) -> tensor
     return %1 : tensor<3x3xi64>
 }
 
-// CHECK-LABEL: @test_shard_propagate_gap
-func.func @test_shard_propagate_gap(%arg0: tensor<1024x1024xi64>) -> tensor<3x3xi64> {
+// CHECK-LABEL: @test_shard_propagate_subview_gap
+func.func @test_shard_propagate_subview_gap(%arg0: tensor<1024x1024xi64>) -> tensor<3x3xi64> {
     // CHECK: [[S:%.*]] = mesh.sharding @mesh4 split_axes = {{\[\[}}0]] : !mesh.sharding
     %s = mesh.sharding @mesh4 split_axes = [[0]] : !mesh.sharding
     // CHECK: mesh.shard %arg0 to [[S]] : tensor<1024x1024xi64>
@@ -58,4 +58,17 @@ func.func @test_shard_propagate_gap(%arg0: tensor<1024x1024xi64>) -> tensor<3x3x
     return %1 : tensor<3x3xi64>
 }
 
+// CHECK-LABEL: @test_shard_propagate_insert_slice
+func.func @test_shard_propagate_insert_slice(%arg0: tensor<1024x1024xi64>, %arg1: tensor<3x3xi64>) {
+    %s = mesh.sharding @mesh4 split_axes = [[0]] : !mesh.sharding
+    %0 = mesh.shard %arg0 to %s : tensor<1024x1024xi64>
+    // CHECK: mesh.sharding
+    // CHECK: mesh.sharding
+    // CHECK: mesh.sharding
+    // CHECK: %[[sharding_2:.*]] = mesh.sharding @mesh4 split_axes = {{\[\[}}0]] sharded_dims_sizes = [3, 0, 0, 0] : !mesh.sharding
+    // CHECK: %[[sharding_annotated_4:.*]] = mesh.shard %sharding_annotated_3 to %[[sharding_2]] annotate_for_users : tensor<3x3xi64>
+    // CHECK-NEXT: ndarray.insert_slice %[[sharding_annotated_4]] into
+    ndarray.insert_slice %arg1 into %0[0, 0][3, 3][1, 1] : tensor<3x3xi64> into tensor<1024x1024xi64>
+    return
+}
 }
